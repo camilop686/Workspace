67a732a02979182a5a5ceb48260dbeceff6c3e10
CAMILO <juanpinedac@ielacandelariamedellin.edu.co>
2026-05-14 19:38:23 +0000

    calculator

---
+++ 13-05/calculadora-funciones/app.js
@@ -0,0 +1,50 @@
+function sumar(a , b) {
+    resultado = a + b;
+    return resultado;
+}
+
+function restar(a , b) {
+    resultado = a - b;
+    return resultado;
+}
+
+function multiplicar(a , b) {
+    resultado = a * b;
+    return resultado;
+}
+
+function dividir(a , b) {
+    resultado = a / b;
+    return resultado;
+}
+
+function potencia(a , b) {
+    resultado = a ** b;
+    return resultado;
+}
+
+console.log("Resultado suma:");
+console.log(sumar(10, 5));
+
+console.log("----------------");
+
+console.log("Resultado resta:");
+console.log(restar(10, 5));
+
+console.log("----------------");
+
+console.log("Resultado multiplicación:");
+console.log(multiplicar(10, 5));
+
+console.log("----------------");
+
+console.log("Resultado división:");
+console.log(dividir(10, 5));
+
+console.log("----------------");
+
+console.log("Resultado potencia:");
+console.log(potencia(10, 5));
+
+console.log("----------------");
+
